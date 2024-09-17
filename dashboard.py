80d0ea80aaca646700be0f74ea383c4e6df7c883
Paulo Silva <rogeriosprf@users.noreply.github.com>
2024-09-17 17:20:29 -0300

    Final

--- dashboard.py
+++ dashboard.py
@@ -11,12 +11,20 @@ app = dash.Dash(__name__)
 df_porAno = pd.read_excel("dados/arquivo final/receitas.xlsx", sheet_name="Total por Ano")
 df_porCategoria = pd.read_excel("dados/arquivo final/receitas.xlsx", sheet_name="Total por Categoria")
 df_porOrigem = pd.read_excel("dados/arquivo final/receitas.xlsx", sheet_name="Total por Origem")
+df_realizadoxprevisto = pd.read_excel("dados/arquivo final/receitas.xlsx", sheet_name="Realizado x Previsto")
+df_melted = df_realizadoxprevisto.melt(id_vars='ANO EXERCÍCIO', value_vars=['VALOR REALIZADO', 'VALOR PREVISTO ATUALIZADO'],
+                           var_name='Tipo', value_name='Valor')
 
 grafico_porAno  = px.line(df_porAno, x="ANO EXERCÍCIO", y="VALOR REALIZADO", text="VALOR REALIZADO", title="Total por ano")
 grafico_porAno.update_traces(textposition='top center')
 
 grafico_porCategoria = px.bar(df_porCategoria, x="CATEGORIA ECONÔMICA", y="VALOR REALIZADO", title="Total por Categoria Econômica")
+
 grafico_porOrigem = px.bar(df_porOrigem, x="ORIGEM RECEITA", y="VALOR REALIZADO", title="Total por Origem da Receita")
+
+grafico_previsto_vs_realizado = px.line(df_melted, x="ANO EXERCÍCIO", y="Valor", color='Tipo', 
+                                        text="Valor", title="Total do Valor Previsto vs Realizado por Ano")
+grafico_previsto_vs_realizado.update_traces(textposition='top center')
 
 # Define o app
 app.layout = html.Div(children=[
@@ -33,10 +41,15 @@ app.layout = html.Div(children=[
     ),
 
     dcc.Graph(
-        id="TotalporCategoria",
+        id="TotalporOrigem",
         figure=grafico_porOrigem
-    ),
-    
+    ),    
+
+    # Novo gráfico com as linhas de valor previsto x valor realizado
+    dcc.Graph(
+        id="PrevistoVsRealizado",
+        figure=grafico_previsto_vs_realizado
+    )
 ])
 
 if __name__ == '__main__':
--- etl.py
+++ etl.py
@@ -11,23 +11,26 @@ pd.set_option('display.float_format', '{:.2f}'.format)
 df_combinado['VALOR REALIZADO'] = df_combinado['VALOR REALIZADO'].str.replace(',','.')
 df_combinado['VALOR REALIZADO'] = pd.to_numeric(df_combinado['VALOR REALIZADO'])
 
+df_combinado['VALOR PREVISTO ATUALIZADO'] = df_combinado['VALOR PREVISTO ATUALIZADO'].str.replace(',','.')
+df_combinado['VALOR PREVISTO ATUALIZADO'] = pd.to_numeric(df_combinado['VALOR PREVISTO ATUALIZADO'])
+
 # Agrupando e somando [VALOR REALIZADO] por [ANO]
 TotalporAno = df_combinado.groupby('ANO EXERCÍCIO')['VALOR REALIZADO'].sum().reset_index()
 
 # Total por Categoria
 TotalporCategoria = df_combinado.groupby('CATEGORIA ECONÔMICA')['VALOR REALIZADO'].sum().reset_index()
 
-# Total de casos por estado
+# Total por Origem
 TotalporOrigem = df_combinado.groupby('ORIGEM RECEITA')['VALOR REALIZADO'].sum().reset_index()
 
-# # Total de bitos por estado
-# obitosNovosEstado = df_combinado.groupby('estado')['obitosNovos'].sum().reset_index()
+# Total por Ano Previsto x Realizado
+TotalporAnoRealizadoxPrevisto = df_combinado.groupby('ANO EXERCÍCIO')[['VALOR REALIZADO','VALOR PREVISTO ATUALIZADO']].sum().reset_index()
 
 
 with pd.ExcelWriter('dados/arquivo final/receitas.xlsx', engine='openpyxl') as writer:
      TotalporAno.to_excel(writer, sheet_name="Total por Ano", index=False)
      TotalporCategoria.to_excel(writer, sheet_name="Total por Categoria", index=False)
      TotalporOrigem.to_excel(writer, sheet_name="Total por Origem", index=False)
-#     # obitosNovosEstado.to_excel(writer, sheet_name="Total de obitos por estado", index=False)
+     TotalporAnoRealizadoxPrevisto.to_excel(writer, sheet_name="Realizado x Previsto", index=False)
 
 print('Arquivo gerado com sucesso')
